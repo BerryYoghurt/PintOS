ead15b4422ccd0018c0d976efc7fc6d57873207f
BerryYoghurt <jehadaly@gmail.com>
2021-06-13 13:22:48 -0700

    need to check something in original code

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -93,8 +93,9 @@ timer_sleep (int64_t ticks)
   int64_t start = timer_ticks ();
 
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+
+  printf("<timer_sleep>sleep for %lld ticks\n", ticks);
+  thread_sleep(start,  ticks);
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
